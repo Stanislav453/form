b477addb6b98c6468682edb64887d2b6540e7f0a
Stanislav Jendrišák <st.jendrisak@gmail.com>
2023-04-10 23:32:18 +0200

    add DotContainer.tsx

---
+++ src/component/styled/DotContainer.tsx
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const DotContainer = styled.div`
+  width: 100%;
+  height: auto;
+
+  position: absolute;
+  left: 0;
+  bottom: 25px;
+
+  display: flex;
+  justify-content: center;
+  gap: .5em;
+`;
