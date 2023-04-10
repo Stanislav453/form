6fcae6ae257f3ce83f64dc257e556deee361e545
Stanislav Jendrišák <st.jendrisak@gmail.com>
2023-04-10 23:30:19 +0200

    rewrite title

--- src/Data.ts
+++ src/Data.ts
@@ -5,23 +5,23 @@ import img_four from "./img/image_four.webp";
 
 export const Data = [
   {
-    title: "Lorem ipsum dolor sit amet",
+    title: "Sit amet",
     img: img_one,
     desc: "onsectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.",
   },
   {
-    title: "Suscipit tellus mauri",
+    title: "Suscipit mauri",
     img: img_two,
     desc: "Feugiat nibh sed pulvinar proin gravida hendrerit. Lorem donec massa sapien faucibus et molestie ac feugiat. ",
   },
   {
-    title: "Purus sit amet volutpat ",
+    title: "Purus volutpat ",
     img: img_three,
     desc: "Consequat mauris nunc. Lorem donec massa sapien faucibus et molestie ac feugiat sed",
   },
   {
-    title: "Lectus arcu bibendum at varius vel",
+    title: "Arcu bibenduml",
     img: img_four,
-    desc: "Vitae nunc sed velit dignissim sodales ut eu sem. Nulla pellentesque dignissim enim sit. Arcu vitae elementum curabitur vitae nunc sed. Tempus quam pellentesque nec nam aliquam sem.",
+    desc: "Vitae nunc sed velit dignissim sodales ut eu sem. Nulla pellentesque dignissim enim sit. Arcu vitae elementum curabitur vitae nunc sed.",
   },
 ];
