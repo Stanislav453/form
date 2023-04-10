cd1f48f0026768d7eff28f4312d856d79fe8ed2e
Stanislav Jendrišák <st.jendrisak@gmail.com>
2023-04-10 16:22:03 +0200

    create styled Container.tsx

---
+++ src/component/styled/Container.tsx
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+    width: 100%;
+    height: auto;
+
+    display: flex;
+    flex-direction: column;
+    gap: 1em;
+
+    padding: 3em 0;
+`;
