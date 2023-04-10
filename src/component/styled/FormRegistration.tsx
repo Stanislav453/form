590de39a9d67206fec4f869d1fdf245473e4613f
Stanislav Jendrišák <st.jendrisak@gmail.com>
2023-04-10 23:33:02 +0200

    add FormRegistration

---
+++ src/component/styled/FormRegistration.tsx
@@ -0,0 +1,26 @@
+import styled from "styled-components";
+
+export const FormRegistration = styled.div`
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  gap: 1em;
+
+  padding-bottom: 1em;
+
+  color: #fff;
+  background-image: linear-gradient(
+    to top,
+    #767872,
+    #51625a,
+    #284b4d,
+    #003346,
+    #051937
+  );
+  h1 {
+    display: block;
+    text-align: center;
+    padding: 0.5em 0;
+    font-size: clamp(1.5em, 10vw, 3.5em);
+  }
+`;
