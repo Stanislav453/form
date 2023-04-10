debc04931015938df8ffa01191a8bd2356f9abae
Stanislav Jendrišák <st.jendrisak@gmail.com>
2023-04-10 16:21:50 +0200

    create styled CheckBoxContainer.tsx

---
+++ src/component/styled/CheckBoxContainer.tsx
@@ -0,0 +1,15 @@
+import styled from "styled-components"
+
+export const CheckBoxContainer = styled.div`
+  width: 100%;
+  height: auto;
+
+  display: flex;
+  gap: 1.3em;
+
+  padding: 0 3em;
+
+  label > p {
+    line-height: 1.5;
+  }
+`;
