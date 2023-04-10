249f2dcbc1a96517b1daec8a5c11971e2dbb48cd
Stanislav Jendrišák <st.jendrisak@gmail.com>
2023-04-10 16:22:52 +0200

    create styled Global.tsx

--- src/component/styled/Global.tsx
+++ src/component/styled/Global.tsx
@@ -8,17 +8,17 @@ export const Global = createGlobalStyle`
     }
 
     body {
-        width: 100%;
-        height: 100vh;
-
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        justify-content: center;
 
         font-family: Verdana, Geneva, Tahoma, sans-serif;
         font-size: 1em;
         color: #000;
-         background-image: linear-gradient(to top, #051937, #003346, #284b4d, #51625a, #767872);
+          background-image: linear-gradient(
+    to top,
+    #051937,
+    #003346,
+    #284b4d,
+    #51625a,
+    #767872
+  );
     }
 `;
