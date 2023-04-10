72ab9c3d731a7e9fe9bc6a76f384ce8742ce5e8f
Stanislav Jendrišák <st.jendrisak@gmail.com>
2023-04-10 16:19:48 +0200

    add <Form />

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { Global } from "./component/styled/Global";
+import { Form } from "./component/Form";
 
 export const App = () => {
   return (
     <>
       <Global />
+      <Form/>
     </>
   );
 };
