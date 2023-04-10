a3fc4640be41bcba500d8511d94cb43b03088568
Stanislav Jendrišák <st.jendrisak@gmail.com>
2023-04-10 16:22:40 +0200

    create styled FormStyled.tsx

---
+++ src/component/styled/FormStyle.tsx
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const FormStyle = styled.form`
+  width: 100%;
+  max-width: 1200px;
+  height: auto;
+
+  display: flex;
+  flex-wrap: wrap;
+  flex-direction: row;
+`;
