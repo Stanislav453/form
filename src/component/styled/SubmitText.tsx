da42856d7dad62d8f7cb5403ae61391b8549cca1
Stanislav Jendrišák <st.jendrisak@gmail.com>
2023-04-11 18:50:10 +0200

    Create SubmitText

---
+++ src/component/styled/SubmitText.tsx
@@ -0,0 +1,12 @@
+import styled from 'styled-components'
+
+export const SubmitText = styled.p`
+    font-size: 2em;
+    text-align: center;
+    margin: 0 1em;
+    vertical-align: auto;
+
+    span {
+        font-weight: 900;
+    }
+`;
