d39f7a772e2c3229d6d0adabd4b2240880f5f62b
Stanislav Jendrišák <st.jendrisak@gmail.com>
2023-04-13 19:52:46 +0200

    change padding

--- src/component/styled/Container.tsx
+++ src/component/styled/Container.tsx
@@ -1,12 +1,12 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-    width: 100%;
-    height: auto;
+  width: 100%;
+  height: auto;
 
-    display: flex;
-    flex-direction: column;
-    gap: 1em;
+  display: flex;
+  flex-direction: column;
+  gap: 1em;
 
-    padding: 3em 0;
+  padding: 1em 0;
 `;
