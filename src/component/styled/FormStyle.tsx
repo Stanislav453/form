f7a705998e912d33233b0f2ff3098adccb1db6df
Stanislav Jendrišák <st.jendrisak@gmail.com>
2023-04-13 19:53:15 +0200

    fix bug width box

--- src/component/styled/FormStyle.tsx
+++ src/component/styled/FormStyle.tsx
@@ -3,9 +3,15 @@ import styled from "styled-components";
 export const FormStyle = styled.form`
   width: 100%;
   max-width: 1200px;
-  height: min(800px);
+  height: auto;
 
   display: flex;
   flex-wrap: wrap;
   flex-direction: row;
+  margin: 0 1em;
+
+  
+  @media ( max-width: 50em ) {
+    flex-direction: column;
+  }
 `;
