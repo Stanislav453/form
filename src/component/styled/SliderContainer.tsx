f2c5dff9bf2c40638c209db04fe2919b7ec016d1
Stanislav Jendrišák <st.jendrisak@gmail.com>
2023-04-17 03:13:40 +0200

    add @media and changed style

--- src/component/styled/SliderContainer.tsx
+++ src/component/styled/SliderContainer.tsx
@@ -2,9 +2,13 @@ import styled from "styled-components";
 
 export const SliderContainer = styled.div`
   width: 100%;
-  height: 100%;
+  min-height: 800px;
 
   position: relative;
   top: 0;
   left: 0;
+
+  @media (max-width: 50em /* 800px */) {
+    height: 100vh;
+  }
 `;
