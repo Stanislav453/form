2488a42384b70abeba14a40c8751c2748f8dca9d
Stanislav Jendrišák <st.jendrisak@gmail.com>
2023-04-17 03:05:59 +0200

    create style

---
+++ src/component/styled/ButtonSliderContainer.tsx
@@ -0,0 +1,15 @@
+import styled from 'styled-components'
+
+export const ButtonSliderContainer = styled.div`
+width: 100%;
+height: auto;
+
+position: absolute;
+bottom: 100px;
+left: 0;
+z-index: 1000;
+
+display: flex;
+align-items: center;
+justify-content: center;
+`
