9b3e4618b9ff8b8b2ac3a3fe730c6f8ea83ee30b
Stanislav Jendrišák <st.jendrisak@gmail.com>
2023-04-17 03:10:30 +0200

    removing unnecessary code and styled input,button,select,a

--- src/component/styled/Global.tsx
+++ src/component/styled/Global.tsx
@@ -2,24 +2,25 @@ import { createGlobalStyle } from "styled-components";
 
 export const Global = createGlobalStyle`
     * {
-        padding: 0;
-        margin: 0;
-        box-sizing: border-box;
+      padding: 0;
+      margin: 0;
+      box-sizing: border-box;
+    }
+
+    a {
+      color: #fff;
+      text-decoration: none;
+    }
+
+    input,button,select {
+      border-radius: 10px;
     }
 
     body {
-
-        font-family: Verdana, Geneva, Tahoma, sans-serif;
-        font-size: 1em;
-        margin: 1em;
-        color: #000;
-          background-image: linear-gradient(
-    to top,
-    #051937,
-    #003346,
-    #284b4d,
-    #51625a,
-    #767872
-  );
+      font-family: Verdana, Geneva, Tahoma, sans-serif;
+      font-size: 1em;
+      color: #000;
+      background-color: #003346;
     }
+    
 `;
